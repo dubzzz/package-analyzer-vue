b4656944da13567e460fd688c388462d18344bf8
Nicolas DUBIEN <github@dubien.org>
2019-07-30 01:01:01 +0200

    Better base for VueRouter

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue';
-import { baseUrl } from '../vue.config'
 
 const SearchPage = () => import('./components/SearchPage.vue');
 const DetailsPage = () => import('./components/DetailsPage.vue');
@@ -13,22 +12,8 @@ Vue.use(VueRouter)
 Vue.config.productionTip = false
 Vue.config.devtools = true
 
-const extractBase = (url) => {
-  try {
-    const u = new URL(url);
-    // eslint-disable-next-line no-console
-    console.log(`(success) Extracted base url: `, u.pathname);
-    return u.pathname;
-  }
-  catch (err) {
-    // eslint-disable-next-line no-console
-    console.log(`(failure) Extracted base url: `, url);
-    return url;
-  }
-}
-
 const router = new VueRouter({
-  base: extractBase(baseUrl),
+  base: process.env.NODE_ENV === 'production' ? '/package-analyzer-vue/' : '/',
   mode: 'history',
   routes: [
     { path: '/', name: 'search', component: SearchPage },
