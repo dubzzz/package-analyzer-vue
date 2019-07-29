380dede0dcacf2a0f1cd30ac9da43db378178a51
Nicolas DUBIEN <github@dubien.org>
2019-07-29 23:34:24 +0200

    Add loading count

--- src/components/DetailsPage.vue
+++ src/components/DetailsPage.vue
@@ -9,6 +9,11 @@
       Details for
       <em>{{packageName}}</em>
     </h2>
+    <p v-if="numRemaining > 0">
+      Currently loading {{numRemaining}} package(s)...
+      <br />
+      Over {{numNodes+numRemaining}} detected
+    </p>
     <svg width="400" height="300" />
   </div>
 </template>
@@ -28,10 +33,51 @@ export default {
   props: {
     packageName: String
   },
-  data: () => ({ nodes: [], links: [], packages: {} }),
+  data: () => ({
+    cleanD3: () => {},
+    packages: {},
+    numNodes: 0,
+    numRemaining: 0
+  }),
   methods: {
     backHome: function() {
       this.$router.push({ name: "search", params: {} });
+    },
+    drawGraph: function(nodes, links) {
+      this.cleanD3();
+      const simulation = d3
+        .forceSimulation(nodes)
+        .force("link", d3.forceLink(links).id(l => l.id))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(400 / 2, 300 / 2));
+      const svg = d3.select("svg");
+      const link = svg
+        .append("g")
+        .attr("stroke", "#777")
+        .selectAll("line")
+        .data(links)
+        .join("line");
+      const node = svg
+        .append("g")
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("r", 3)
+        .attr("fill", "blue");
+      const title = node.append("title").text(n => n.id);
+      simulation.on("tick", () => {
+        link
+          .attr("x1", l => l.source.x)
+          .attr("y1", l => l.source.y)
+          .attr("x2", l => l.target.x)
+          .attr("y2", l => l.target.y);
+        node.attr("cx", n => n.x).attr("cy", n => n.y);
+      });
+      this.cleanD3 = () => {
+        link.remove();
+        node.remove();
+        title.remove();
+      };
     }
   },
   watch: {
@@ -40,22 +86,28 @@ export default {
       handler: async function(newPackageName) {
         const nodes = [];
         const links = [];
-        const visited = {};
+        const visited = { [newPackageName]: true };
+        nodes.push({ id: newPackageName, group: 1 });
         const remaining = [newPackageName];
-        while (remaining.length !== 0) {
-          const [currentPackage] = remaining.splice(-1);
-          if (visited[currentPackage] === true) continue;
-          visited[currentPackage] = true;
 
+        while (remaining.length !== 0) {
+          this.numNodes = nodes.length;
+          this.numRemaining = remaining.length;
+
+          const [currentPackage] = remaining.splice(-1);
           const packageDeps =
             this.packages[currentPackage] ||
             ((await deps(currentPackage)).collected.metadata.dependencies ||
               {});
           this.packages[currentPackage] = packageDeps;
 
-          nodes.push({ id: currentPackage, group: 1 });
           for (const requirement in packageDeps) {
-            remaining.push(requirement);
+            if (visited[requirement] !== true) {
+              visited[requirement] = true;
+              remaining.push(requirement);
+              nodes.push({ id: requirement, group: 1 });
+            }
+
             links.push({
               source: currentPackage,
               target: requirement,
@@ -64,38 +116,11 @@ export default {
           }
 
           if (this.packageName !== newPackageName) return;
-
-          this.nodes = nodes;
-          this.links = links;
         }
-        const simulation = d3
-          .forceSimulation(nodes)
-          .force("link", d3.forceLink(links).id(l => l.id))
-          .force("charge", d3.forceManyBody())
-          .force("center", d3.forceCenter(400 / 2, 300 / 2));
-        const svg = d3.select("svg");
-        const link = svg
-          .append("g")
-          .attr("stroke", "#777")
-          .selectAll("line")
-          .data(links)
-          .join("line");
-        const node = svg
-          .append("g")
-          .selectAll("circle")
-          .data(nodes)
-          .join("circle")
-          .attr("r", 3)
-          .attr("fill", "blue");
-        node.append("title").text(n => n.id);
-        simulation.on("tick", () => {
-          link
-            .attr("x1", l => l.source.x)
-            .attr("y1", l => l.source.y)
-            .attr("x2", l => l.target.x)
-            .attr("y2", l => l.target.y);
-          node.attr("cx", n => n.x).attr("cy", n => n.y);
-        });
+
+        this.numNodes = nodes.length;
+        this.numRemaining = 0;
+        this.drawGraph(nodes, links);
       }
     }
   }
